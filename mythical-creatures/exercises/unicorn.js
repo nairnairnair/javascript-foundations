abc84d7f5cb403c9975b7e623389b0ff922e1c9c
emmacintosh <emmammrussell@gmail.com>
2022-05-17 21:05:16 -0600

    add unicorn and test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,19 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+  }
+  //console.log(constructor)
+  isWhite() {
+    if (this.color === 'white') {
+      return true
+    } else {
+return false
+    }
+
+    }
+    says(words) {
+      return '**;* ' + words + ' *;**'
   }
 }
 
